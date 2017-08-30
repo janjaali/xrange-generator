0362cb00340476a6926afe078402353b2841f8eb
Siyavash Habashi <ghashange@live.com>
2017-08-30 21:00:19 +0200

    build via webpack

---
+++ build/index.js
@@ -0,0 +1,148 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["xrange-generator"] = factory();
+	else
+		root["xrange-generator"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _marked = /*#__PURE__*/regeneratorRuntime.mark(xrange);
+
+function xrange(from, to, by) {
+  var start, end, step;
+  return regeneratorRuntime.wrap(function xrange$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          start = from || 0;
+          end = to;
+          step = by || 1;
+
+          if (!(end && end < start)) {
+            _context.next = 12;
+            break;
+          }
+
+        case 4:
+          if (!(start !== end - 1)) {
+            _context.next = 10;
+            break;
+          }
+
+          _context.next = 7;
+          return start;
+
+        case 7:
+          start -= step;
+          _context.next = 4;
+          break;
+
+        case 10:
+          _context.next = 18;
+          break;
+
+        case 12:
+          if (!(start !== end + 1)) {
+            _context.next = 18;
+            break;
+          }
+
+          _context.next = 15;
+          return start;
+
+        case 15:
+          start += step;
+          _context.next = 12;
+          break;
+
+        case 18:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _marked, this);
+}
+
+exports.default = xrange;
+
+/***/ })
+/******/ ]);
+});
